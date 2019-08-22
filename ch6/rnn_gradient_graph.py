2267fc0d5d6498ae9358269bf97fbd53c0373b4f
Takahiro Yamazaki <yamazaki@nvs-one.com>
2019-08-22 16:57:40 +0900

    forwardd learning

--- ch6/rnn_gradient_graph.py
+++ ch6/rnn_gradient_graph.py
@@ -5,12 +5,20 @@ N = 2
 H = 3
 T = 20
 
-dh - np.ones((N,H))
+dh = np.ones((N,H))
 np.random.seed(3)
-Wh = np.random.randn(H,H)
+Wh = np.random.randn(H,H) * 0.5
 
 norm_list = []
 for t in range(T):
     dh = np.dot(dh, Wh.T)
     norm = np.sqrt(np.sum(dh**2)) / N
     norm_list.append(norm)
+
+
+# グラフの描画
+x = np.arange(len(norm_list))
+plt.plot(x, norm_list, label='train')
+plt.xlabel('epochs')
+plt.ylabel('norm')
+plt.show()
